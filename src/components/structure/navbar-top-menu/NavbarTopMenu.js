b372c710c9cf572d34a41f45b6a1f520c19eb7d6
alexandermendo <alexamp1987@gmail.com>
2021-11-25 18:27:42 -0500

    Navbar Actualizado Marca Personal

--- src/components/structure/navbar-top-menu/NavbarTopMenu.js
+++ src/components/structure/navbar-top-menu/NavbarTopMenu.js
@@ -24,6 +24,18 @@ export const NavbarTopMenu = () => {
 
           <div className="collapse navbar-collapse ">
             <ul className="navbar-nav ms-auto">
+              <form className="form-inline my-2 my-lg-0">
+                <input
+                  className="form-control mr-sm-2"
+                  type="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                />
+                <button className="btn my-2 my-sm-0 " type="submit">
+                  <FiSearch color="white" />
+                </button>
+              </form>
+
               <li>
                 <a className="menu nav-link text-white" href="/">
                   INICIO
@@ -39,88 +51,14 @@ export const NavbarTopMenu = () => {
                   CONTACTO
                 </a>
               </li>
+
+              <button className="ini my-2 my-sm-0 " type="submit">
+                Iniciar Sesión
+              </button>
             </ul>
           </div>
-
-          {/* <div className="col">
-            <form action="" className="search-bar d-flex">
-              <input type="search" name="search" pattern=".*\S.*" required />
-              <button className="search-btn" type="submit">
-                <FiSearch />
-              </button>
-            </form>
-          </div> */}
         </div>
       </nav>
-      {/* <nav className=" menutop navbar">
-        <div className="container-fluid">
-          <a class="navbar-brand">Navbar</a>
-          <form class="d-flex">
-            <input
-              class="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button class="btn btn-outline-success" type="submit">
-              Search
-            </button>
-          </form>
-        </div>
-      </nav> */}
-
-      {/* <nav className="menutop navbar-default">
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col">
-              <a className="navbar-brand" href="/">
-                <img
-                  className="image-per"
-                  src="../../AM.png"
-                  alt="logo-personal"
-                  width="100"
-                  height="100"
-                />
-              </a>
-            </div>
-
-            <div className="col">
-              <form action="" className="search-bar d-flex">
-                <input type="search" name="search" pattern=".*\S.*" required />
-                <button class="search-btn" type="submit">
-                  <FiSearch />
-                </button>
-              </form>
-            </div>
-
-            <div className="col">
-              <div className="nav-item collapse navbar-collapse">
-                <ul className="navbar-nav me-auto">
-                  <li >
-                    <a className="nav-link" href="/">
-                      INICIO
-                    </a>
-                  </li>
-                  <li >
-                    <a className="nav-link" href="/">
-                      ACERCA DE MI
-                    </a>
-                  </li>
-                  <li >
-                    <a className="nav-link" href="/">
-                      CONTACTO
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-
-            <div className="col">
-              <button className="iniciar-sesion">Iniciar Sesión</button>
-            </div>
-          </div>
-        </div>
-      </nav> */}
     </>
   );
 };
