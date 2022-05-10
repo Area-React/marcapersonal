d666088f72ac1a93007bf4dabb3e9bb2f45b6a50
Alexander <alexamp1987@gmail.com>
2022-05-10 18:30:20 -0500

    Ajustes Marca Personal 10/05/2022 18:30

--- src/components/structure/navbar-top-menu/NavbarTopMenu.js
+++ src/components/structure/navbar-top-menu/NavbarTopMenu.js
@@ -1,64 +1,108 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./navbarTopMenu.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { FiMenu, FiSearch } from "react-icons/fi";
 
 export const NavbarTopMenu = () => {
+  const [navbar, setNavbar] = useState(false);
+  const [ocultar, setOcultar] = useState(false);
+
+  useEffect(() => {
+    changeColorNavbar();
+    window.addEventListener("scroll", changeColorNavbar);
+  }, []);
+
+  const changeColorNavbar = () => {
+    if (window.scrollY >= 50) {
+      setNavbar(true);
+    } else {
+      setNavbar(false);
+    }
+  };
+
+  const ocultarMenu = () => {
+    setOcultar(!ocultar);
+  };
+
   return (
     <>
-      <nav className="menutop navbar navbar-expand-lg navbar-light">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="/">
-            <img
-              className="image-per"
-              src="../../AM.png"
-              alt="logo-personal"
-              width="100"
-              height="100"
-            />
-          </a>
+      <div
+        className={
+          navbar
+            ? "container-navbarr bg-dark fixed-top "
+            : "container-navbarr fixed-top "
+        }
+      >
+        <div className="row p-0 m-0">
+          <div className="col-lg-2 p-0 m-0 text-center">
+            <a className="navbar-brand-1" href="/">
+              <img src="../../AM.png" alt="Taxi" height="70" />
+            </a>
+            <button
+              className="navbar-toggler ocultar-big-screen"
+              type="button"
+              onClick={ocultarMenu}
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+            >
+              <i className="icon-menu fas fa-bars"></i>
+            </button>
+          </div>
 
-          <button className="navbar-toggler text-white">
-            <FiMenu />
-          </button>
-
-          <div className="collapse navbar-collapse ">
-            <ul className="navbar-nav ms-auto">
-              <form className="form-inline my-2 my-lg-0">
-                <input
-                  className="form-control mr-sm-2"
-                  type="search"
-                  placeholder="Search"
-                  aria-label="Search"
-                />
-                <button className="btn my-2 my-sm-0 " type="submit">
-                  <FiSearch color="white" />
-                </button>
-              </form>
-
-              <li>
-                <a className="menu nav-link text-white" href="/">
+          <div
+            className={
+              ocultar
+                ? "col-lg-7 p-0 m-0 align-self-center ocultar"
+                : "col-lg-7 p-0 m-0 align-self-center"
+            }
+          >
+            <div className="row m-0 p-0">
+              <div className="col-lg-4 text-center align-self-center">
+                <a
+                  className="nav-link text-white text-center align-self-center"
+                  href="/"
+                >
                   INICIO
                 </a>
-              </li>
-              <li>
-                <a className="menu nav-link text-white" href="/">
+              </div>
+              <div className="col-lg-4 text-center align-self-center">
+                <a
+                  className="nav-link text-white text-center align-self-center"
+                  href="/"
+                >
                   ACERCA DE MI
                 </a>
-              </li>
-              <li>
-                <a className="menu nav-link text-white" href="/">
+              </div>
+              <div className="col-lg-4 text-center align-self-center">
+                <a
+                  className="nav-link text-white text-center align-self-center"
+                  href="/"
+                >
                   CONTACTO
                 </a>
-              </li>
+              </div>
+            </div>
+          </div>
 
-              <button className="ini my-2 my-sm-0 " type="submit">
-                Iniciar Sesión
-              </button>
-            </ul>
+          <div
+            className={
+              ocultar
+                ? "col-lg-3 p-0 m-0 align-self-center ocultar"
+                : "col-lg-3 p-0 m-0 align-self-center"
+            }
+          >
+            <div className="row m-0 p-0">
+              <div className=" col-12 col-xl-12 col-sm-12 m-0 p-0 container-button">
+                <div className="btn-ini">
+                  <button className="ini" type="submit">
+                    Iniciar Sesión
+                  </button>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-      </nav>
+      </div>
     </>
   );
 };
